269c8934a9c4e1a8927bc38a3122f40195a6b232
suraj sakhare <sakharesuraj10@gmail.com>
2024-11-02 18:12:39 +0530

    eslintric updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Hero from './components/Sections/HeroSection/hero'
 import Features from './components/Sections/Features/features'
 import Pricing from './components/Sections/Pricing/pricing'
-import PageFeature from './components/Sections/Features/pagefeature'
 import Faq from './components/Sections/Faq/faq'
 
 function page() {
   return (
     <div className='bg-white dark:bg-black'>
       <Hero/>
-      {/* <PageFeature/> */}
       <Features/>
       <Pricing/>
       <Faq/>
