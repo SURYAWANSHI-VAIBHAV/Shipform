6f5a3c063023cca7c8f00850a84154bebd3ef8dc
suraj sakhare <sakharesuraj10@gmail.com>
2024-11-02 20:06:39 +0530

    auth pages added

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import heroimage from './../../../../public/Hero.png'
 
 function page() {
   return (
-    <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+    <div className="flex items-center min-h-screen p-6 pt-10 bg-gray-50 dark:bg-gray-900">
       <div
         className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
       >
         <div className="flex flex-col overflow-y-auto md:flex-row">
-          <div className="h-32 md:h-auto md:w-1/2 border">
+          <div className="h-32 md:h-auto md:w-1/2">
             <img
               aria-hidden="true"
               className="object-cover w-full h-full dark:hidden"
-              src={heroimage.src}
+              src="../assets/img/create-account-office.jpeg"
               alt="Office"
             />
             <img
               aria-hidden="true"
               className="hidden object-cover w-full h-full dark:block"
-              src="../assets/img/login-office-dark.jpeg"
+              src="../assets/img/create-account-office-dark.jpeg"
               alt="Office"
             />
           </div>
@@ -27,12 +26,12 @@ function page() {
               <h1
                 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200"
               >
-                Login
+                Create account
               </h1>
               <label className="block text-sm">
                 <span className="text-gray-700 dark:text-gray-400">Email</span>
                 <input
-                  className="block w-full mt-1 text-sm border-gray-100  dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                  className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                   placeholder="Jane Doe"
                 />
               </label>
@@ -44,12 +43,35 @@ function page() {
                   type="password"
                 />
               </label>
+              <label className="block mt-4 text-sm">
+                <span className="text-gray-700 dark:text-gray-400">
+                  Confirm password
+                </span>
+                <input
+                  className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                  placeholder="***************"
+                  type="password"
+                />
+              </label>
+
+              <div className="flex mt-6 text-sm">
+                <label className="flex items-center dark:text-gray-400">
+                  <input
+                    type="checkbox"
+                    className="text-purple-600 form-checkbox focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
+                  />
+                  <span className="ml-2">
+                    I agree to the
+                    <span className="underline">privacy policy</span>
+                  </span>
+                </label>
+              </div>
 
               <a
                 className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                href="../index.html"
+                href="./login.html"
               >
-                Log in
+                Create account
               </a>
 
               <hr className="my-8" />
@@ -88,17 +110,9 @@ function page() {
               <p className="mt-4">
                 <a
                   className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                  href="./forgot-password.html"
+                  href="./login.html"
                 >
-                  Forgot your password?
-                </a>
-              </p>
-              <p className="mt-1">
-                <a
-                  className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                  href="./create-account.html"
-                >
-                  Create account
+                  Already have an account? Login
                 </a>
               </p>
             </div>
